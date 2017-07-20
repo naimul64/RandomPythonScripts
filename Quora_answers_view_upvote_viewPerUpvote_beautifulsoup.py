c4140ed6c282e0165e94d58b007f401b0d674f0e
naimul <naimul@surecash.net>
2017-07-20 10:12:00 +0600

    Improved Beautiful soup for quora

--- Quora_answers_view_upvote_viewPerUpvote_beautifulsoup.py
+++ Quora_answers_view_upvote_viewPerUpvote_beautifulsoup.py
@@ -1,19 +1,56 @@
 from bs4 import BeautifulSoup
+from operator import itemgetter
 import urllib
-r = urllib.urlopen('file:///home/insan/Documents/zz_GURBAGE/bs.html').read()
+
+
+def round(decimal_place, floating_no):
+    floating_no_backup = floating_no
+
+    if type(floating_no) == type(9):
+        floating_no = float(str(floating_no) + '.0')
+
+    divby2 = 100
+    while divby2 != 0:
+        divby2 = floating_no / 2
+        divby2 = int(divby2)
+        if divby2 >= 1:
+            floating_no -= divby2
+    floating_no -= 1
+
+    for i in range(decimal_place + 1):
+        floating_no *= 10
+    fractionIntoInt = int(floating_no)
+    if fractionIntoInt % 10 > 4:
+        fractionIntoInt = int(fractionIntoInt / 10)
+        fractionIntoInt += 1
+    else:
+        fractionIntoInt = int(fractionIntoInt / 10)
+    back_to_fraction = float(fractionIntoInt)
+    for i in range(decimal_place):
+        back_to_fraction /= 10
+
+    rounded_float = float(int(floating_no_backup))
+    rounded_float += back_to_fraction
+
+    return rounded_float
+
+
+
+r = urllib.urlopen("file:///home/insan/Desktop/gurbaze/What%20is%20your%20most%20powerful%20tip_%20-%20Quora.html").read()
 soup = BeautifulSoup(r,"lxml")
 mydivs = soup.findAll("div", { "class" : "Answer AnswerBase" })
-print len(mydivs)
+print str(len(mydivs)) + " answers found"
 howAmI = []
 for div in mydivs:
-    user = div.findAll("a", { "class" : "user" })[0].string
-    print user.strip
- 
- 
- 
-   
+    try:
+        user = div.findAll("a", { "class" : "user" })[0].string
+    except:
+        user = "ANONYMOUS!!!!!!!!!"
+    # print user.strip
+
     try:
         view = div.findAll("span", { "class" : "meta_num" })[0].string
+
         view = view.strip().replace(",","")
         if  view[-1] == 'k':
             view = view[:-1]
@@ -25,14 +62,14 @@ for div in mydivs:
         #print e
         view = 0
     view = int(str(view))
-    print str(view) + " views"
- 
- 
+    #print str(view) + " views"
+
+
     try:
         upvote = div.findAll("a", { "class" : "VoterListModalLink" })[0].string
         upvote = upvote.strip().replace(",","")
         upvote = upvote.split(' ')[0]
- 
+
         upvote = upvote.strip()
         if  upvote[-1] == 'k':
             upvote = upvote[:-1]
@@ -44,17 +81,29 @@ for div in mydivs:
         #print e
         upvote = 0
     upvote = int(str(upvote))
-    print str(upvote) + " upvotes"
- 
+    # print str(upvote) + " upvotes"
+
     if upvote == 0 :
-        print "No upvotes."
+        # print "No upvotes."
         viewPerUpvote = 100000000000000
     else:
         viewPerUpvote = float(view)/float(upvote)
-        print str(viewPerUpvote) + " views per upvote ===============>\n\n"
- 
-    lisht = [user, view, upvote, viewPerUpvote]
+        # print str(viewPerUpvote) + " views per upvote ===============>\n\n"
+
+
+
+    try:
+        answer_link = div.findAll("a", { "class" : "answer_permalink" })[0]
+        answer_link = answer_link['href']
+
+    except Exception as e:
+        answer_link =""
+
+
+    lisht = [user, view, upvote, round(decimal_place=2, floating_no=viewPerUpvote), answer_link]
     howAmI.append(lisht)
- 
+
+sort_index = 3
+howAmI = sorted(howAmI, key=itemgetter(sort_index))
 for l in howAmI:
     print l
